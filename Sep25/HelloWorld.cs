287c5ffc14c6d72fe0c29033cad5d162b3ba31a8
rnarang <rnarang@LAP-55379>
2023-09-25 16:37:29 +0530

    upto basic of classes

--- Sep25/HelloWorld.cs
+++ Sep25/HelloWorld.cs
@@ -3,11 +3,59 @@
 using System;
 namespace HelloWorld
 {
+    //ANOTHER CLASS
+    /*public class Vehicle
+    {
+        public string Name;
+        public string vehicleName()
+        {
+            Console.WriteLine("Your vehicle name is ");
+            return Name;
+        }
+    }*/
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Hello RITIKA ");
+            // HELLO WORLD//
+            //Console.WriteLine(" Hello RITIKA ");
+
+            //OVERFLOWING//
+            /*checked
+            {
+                int maxInt = int.MaxValue;  // The maximum value for an integer
+                int overflowedInt = maxInt + 1;  // This will cause an overflow
+                Console.WriteLine("Maximum Integer Value: " + maxInt);
+                Console.WriteLine("Overflowed Integer Value: " + overflowedInt);
+            }*/
+
+            //CONVERSION//
+            /*1.
+            string booleanval = "false";
+            bool val2 = Convert.ToBoolean(booleanval);
+            Console.WriteLine(val2);*/
+
+            /*2.
+            string value = "1234";
+            //int numVal = (int)value; // will give error bcs of non compatible types
+            Byte numVal=Convert.ToByte(value); //use try and catch
+            try
+            {
+                string value = "1234";
+                Byte numVal = Convert.ToByte(value);
+                Console.WriteLine(numVal);  
+
+            }
+            catch ( Exception)
+            {
+                Console.WriteLine("This can't be converted");
+            }
+            */
+
+            //CLASS
+           /* var vehicle = new Vehicle();
+            vehicle.Name = "Car";
+            Console.WriteLine(vehicle.vehicleName());*/
         }
     }
 }
