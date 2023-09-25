1c4023111f59e5b3bfa83221b1de5f7f04e74f34
rnarang <rnarang@LAP-55379>
2023-09-25 12:31:39 +0530

    Updated

---
+++ Sep25/Add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sep25
+{
+    internal class Add
+    {
+        public string Name { get; set; }
+    }
+}
