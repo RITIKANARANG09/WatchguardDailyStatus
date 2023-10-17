e9c7ace50d22fda82f51f3d908c92fc39a361676
rnarang <rnarang@LAP-55379>
2023-10-17 21:15:23 +0530

    UPDATE

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace E
+{
+    interface Picture
+    {
+        void photo();
+        void Video();
+
+    }
+    class SadPicture : Picture
+    {
+        public void photo()
+        {
+            Console.WriteLine("SAD PHOTO");
+        }
+        public void Video()
+        {
+            Console.WriteLine("SAD VIDEO");
+        }
+    }
+    class HappyPicture : Picture
+    {
+        public void photo()
+        {
+            Console.WriteLine("HAPPY PHOTO");
+        }
+        public void Video()
+        {
+            Console.WriteLine("HAPPY VIDEO");
+        }
+    }
+    class checker(Picture pic)
+    {
+        pic.photo();
+        pic.video();
+    }
+    class CompInhe
+    {
+        public static void Main(string[] args)
+        {
+            //Picture picture = ;
+            checker c=new checker(new SadPicture());
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public delegate void Calculation(int a, int b);
+namespace Delegate
+{
+    class Q
+    {
+        public static void addition(int a, int b)
+        {
+            Console.WriteLine("Addition : {0}", a + b);
+        }
+        public static void subtraction(int a, int b)
+        {
+            Console.WriteLine("Subtraction : {0}", a - b);
+        }
+        public static void multiplication(int a, int b)
+        {
+            Console.WriteLine("Multiplication : {0}", a * b);
+        }
+        public static void division(int a, int b)
+        {
+            Console.WriteLine("Division : {0}", a / b);
+        }
+    }
+
+    class P:Q
+    {
+        
+        public static void Main(String[] args)
+        {
+            Calculation c = new Calculation(addition);
+            c.Invoke(20,20);
+            c = subtraction;
+            c(20,20);
+            c = multiplication;
+            c(20, 20);
+            c = division;
+            c.Invoke(20, 20);
+        }
+        
+    }
+}
